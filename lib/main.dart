ca5b498c706876ee8cc0776fac1360f27655b549
Anushk Pandey <anushk.pandey2021@vitstudent.ac.in>
2023-09-27 18:42:18 +0530

    first flutter app

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'dart:ffi';
+
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+}
