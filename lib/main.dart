cf16e143e62951870458ade4b30ffea12bcb4b7e
Anushk Pandey <anushk.pandey2021@vitstudent.ac.in>
2023-09-29 14:29:40 +0530

    Added classes for Appstate and nameGenerator

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:ffi';
 
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -10,4 +11,35 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Naming App',
+        theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange)),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    return Scaffold(
+      body: Column(children: [
+        Text('A random Idea:'),
+        Text(appState.current.asLowerCase),
+      ]),
+    );
+  }
 }
